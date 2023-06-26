5d3103efd6975db210004d6933234107e3ea215f
Margaret <maenunezt@gmail.com>
2023-06-25 23:25:46 -0400

    ejercicio siete finalizado o algo así

--- app/src/main/java/com/example/siete/MainActivity.java
+++ app/src/main/java/com/example/siete/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.siete;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initListeners();
+    }
+
+    private void initListeners() {
+        ImageView spotify = findViewById(R.id.spotifybutton);
+        ImageView youtube = findViewById(R.id.youtubebutton);
+        ImageView whatsapp = findViewById(R.id.whatsappbutton);
+        ImageView notes = findViewById(R.id.notesbutton);
+
+        spotify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Bienvenido, siga participando", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        whatsapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Bienvenido, siga participando", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        youtube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Bienvenido, siga participando", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        notes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Bienvenido, siga participando", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
